c497fbf116ecc99b45ea093f7ae753697af4e954
joaoroldaodev <joaoroldaodev@gmail.com>
2026-05-18 21:19:20 -0300

    Ajustando lista de objetos

--- rep_lista_1_ex_2.cpp
+++ rep_lista_1_ex_2.cpp
@@ -19,33 +19,39 @@ int main() {
     string name;
     char resp;
 
-    resp = 'S';
-
     do { 
         name = "";
         age = 0;
 
         cout << "Nome do aluno: ";
         getline(cin, name);
-        cout << "Digite a idade de " << name << "";
+
+        cout << "Digite a idade de " << name << ": ";
         cin >> age;
+        cin.ignore();
 
         if (age == 18 || age > 20) {
             studants.push_back(Studant(name, age));
         }
 
-        do
-        {
+        
+        do {
             cout << "Continuar ? (S/N)";
             resp = toupper(cin.get());
+            cin.ignore();
         } while (resp != 'S' && resp != 'N');
 
         if (resp == 'N') {
             repeat = false;
         }
         
+        cout << endl;
     } while (repeat == true);
+
     
+    for (int i = 0; i < size(studants); i++) {
+        cout << "Nome: " << studants[i].name << ", Idade: " << studants[i].age << endl;
+    };
     
     return 0;
 }
